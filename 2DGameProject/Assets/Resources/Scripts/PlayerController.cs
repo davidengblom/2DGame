297fd0f9f25cfa3bb61426cc6fe345e7f7fda3be
David Engblom <david.engblom@student.forsbergsskola.se>
2020-08-05 21:43:59 +0200

    Hold to jump higher function added
    Will need updating

--- 2DGameProject/Assets/Resources/Scripts/PlayerController.cs
+++ 2DGameProject/Assets/Resources/Scripts/PlayerController.cs
@@ -7,14 +7,19 @@ public class PlayerController : MonoBehaviour
     //Public Variables
     public float moveSpeed = 5f;
     public float jumpHeight = 10f;
-    public float groundCheckRadius = 0.3f;
+    public float jumpTime;
+    
 
     //Private Variables
-    private Vector2 moveInput;
+    private float groundCheckRadius = 0.3f;
+    private float jumpTimeCounter;
     private bool isGrounded;
+    private bool isJumping;
     private bool jump;
-    public LayerMask groundLayer;
-    
+    private Vector2 moveInput;
+    private LayerMask groundLayer;
+
+
     //Components
     private Rigidbody2D rb;
     private Animator animator;
@@ -25,8 +30,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
         groundCheck = GameObject.Find("GroundCheck").transform;
-        groundLayer = 8;
-        Debug.Log(LayerMask.NameToLayer("Ground"));
+        groundLayer = LayerMask.GetMask("Ground");
     }
 
     void FixedUpdate()
@@ -34,12 +38,16 @@ public class PlayerController : MonoBehaviour
         //Move the player
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
 
+        //Jumping
         if(jump)
         {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
             rb.velocity = Vector2.up * jumpHeight;
             jump = false;
         }
 
+        //Animation Parametres
         animator.SetFloat("Horizontal", moveInput.x);
         animator.SetFloat("Speed", moveInput.sqrMagnitude);
         animator.SetBool("Grounded", isGrounded);
@@ -47,12 +55,32 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //Check for ground collision and movement input
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         moveInput.x = Input.GetAxisRaw("Horizontal");
 
+        //Check for jump input
         if(isGrounded && Input.GetButtonDown("Jump"))
         {
             jump = true;
+        }
+
+        if(Input.GetButton("Jump") && isJumping)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpHeight;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
         }
     }
 }
