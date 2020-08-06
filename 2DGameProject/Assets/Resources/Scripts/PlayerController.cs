030dbb405428c66c46ee3dbdc11dcd4e4b8dbab1
David Engblom <david.engblom@student.forsbergsskola.se>
2020-08-06 22:47:43 +0200

    Added jump and walk sounds
    Walk sound isn't currently working, but the jump sound is fine.

--- 2DGameProject/Assets/Resources/Scripts/PlayerController.cs
+++ 2DGameProject/Assets/Resources/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Audio clips
+    [Header("Audio Clips")]
+    public AudioClip jumpSound;
+    public AudioClip walkSound;
+
     //Public variables
+    [Header("Variables")]
     public float moveSpeed = 5f;
     public float jumpHeight = 10f;
     public float jumpTime;
@@ -15,7 +21,7 @@ public class PlayerController : MonoBehaviour
     private float jumpTimeCounter;
     private bool isGrounded;
     private bool isJumping;
-    private bool jump;
+    private bool doJump;
     private Vector2 moveInput;
     private LayerMask groundLayer;
 
@@ -23,13 +29,15 @@ public class PlayerController : MonoBehaviour
     //Components
     private Rigidbody2D rb;
     private Animator animator;
+    private AudioSource audioSource;
     private Transform groundCheck;
 
     void Start()
     {
-        //Assign variables
+        //Assign components & variables
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
+        audioSource = GetComponent<AudioSource>();
         groundCheck = GameObject.Find("GroundCheck").transform;
         groundLayer = LayerMask.GetMask("Ground");
     }
@@ -40,12 +48,17 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
 
         //Jumping
-        if(jump)
+        if(doJump)
         {
+            //Play the jump sound
+            audioSource.clip = jumpSound;
+            audioSource.Play();
+
+            //Have the player jump
             isJumping = true;
             jumpTimeCounter = jumpTime;
             rb.velocity = Vector2.up * jumpHeight;
-            jump = false;
+            doJump = false;
         }
 
         //Animation parametres
@@ -63,7 +76,7 @@ public class PlayerController : MonoBehaviour
         //Check for jump input
         if(isGrounded && Input.GetButtonDown("Jump"))
         {
-            jump = true;
+            doJump = true;
         }
 
         //Check if jump button is held down after jumping
