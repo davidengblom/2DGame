95d227046ab94f925360ed187fa7ed7990cda5fa
David Engblom <kirellen1573@zoho.eu>
2020-08-07 20:59:50 +0200

    Added grass prefab and tweaked volume and layers

---
+++ 2DGameProject/Assets/Resources/Scripts/Environment/Vegetation/Grass/GrassBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassBehaviour : MonoBehaviour
+{
+    //Components
+    private Animator animator;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        animator.SetTrigger("Move");
+        audioSource.Play();
+    }
+}
